3473e0cdb476e599e192600c39eac3f50a423931
leehal <annaleehalim@gmail.com>
2023-04-11 17:59:22 +0900

    0411

--- java_20230411/src/ex5/Exam09.java
+++ java_20230411/src/ex5/Exam09.java
@@ -8,6 +8,7 @@ public class Exam09 {
 	public static void main(String[] args) {
 		
 		System.out.println("1.입금 2.출금 3.잔고 4.종료");
+		System.out.println("---------------------------");
 		
 		
 		Scanner sc = new Scanner(System.in);
@@ -15,20 +16,9 @@ public class Exam09 {
 		int num = sc.nextInt();
 		
 		
-	
-	for(int i=2; i<=9;i++) {
-			for(int j=1; j<=9; j++) {
-				if(i==j) continue;
-				System.out.println(i+" X " +j+ " = "+ i*j);
-		}
-			
-		}	
-		
-		}
-		
-		} 
-
 
 
 	
 
+}
+}
---
+++ java_20230411/src/ex5/Exam11.java
@@ -0,0 +1,44 @@
+package ex5;
+//규율은 날 자유롭게 만든다
+//날 죽이지 못한 고통은 날 강하게 한다.
+//아스키코드표 참고
+import java.util.*;
+public class Exam11 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("몇 번 입력?");
+		
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		int sum=0;
+		int num=0;
+		for(;count-1>=0;count--) {
+			System.out.println("숫자 입력");
+			 num = sc.nextInt();
+				if (count-1>0) {
+					if(num%2==1)
+						continue;
+					else
+						sum+=num;
+				}
+				else 
+					break;
+					
+		
+		}
+		System.out.println("짝수 값의 합은"+ sum);
+		
+		}
+			
+		}	
+		
+		
+		
+		
+
+
+
+
+	
+
