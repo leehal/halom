61907c259e084fa41d2f876820e4ed3256b5bec6
leehal <annaleehalim@gmail.com>
2023-04-25 09:24:50 +0900

    23235

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/83/9017792574e2001d173e93e8c4ee706d
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Admin
+ *
+ */
+module java_20230424 {
+}
---
+++ java_20230424/src/ex01/Exam02.java
@@ -0,0 +1,16 @@
+package ex01;
+
+interface Animal{
+	void eat();
+	public abstract void sleep();
+}
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
